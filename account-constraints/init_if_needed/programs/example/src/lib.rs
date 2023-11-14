fbc454fd81a46949a3e72c43df0afc9db77bf4a2
John <johnzhengyuliu@gmail.com>
2023-11-13 18:19:21 -0600

    update init_if_needed example

--- account-constraints/init_if_needed/programs/example/src/lib.rs
+++ account-constraints/init_if_needed/programs/example/src/lib.rs
@@ -1,4 +1,8 @@
 use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token::{Mint, Token, TokenAccount},
+};
 
 declare_id!("7ZpxAfW6xKiGLjyvvUPB71HQKWTqs3VdeYTY5uT784VE");
 
@@ -7,12 +11,20 @@ pub mod example {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>, input: u64) -> Result<()> {
-        require!(
-            !ctx.accounts.new_account.is_initialized,
-            MyError::AlreadyInitialized
-        );
-        ctx.accounts.new_account.is_initialized = true;
-        ctx.accounts.new_account.data = input;
+        if ctx.accounts.new_account.is_initialized {
+            msg!("Already initialized, data unchanged");
+        } else {
+            ctx.accounts.new_account.is_initialized = true;
+            ctx.accounts.new_account.data = input;
+            msg!("Initializing account with data: {}", input);
+        }
+        Ok(())
+    }
+
+    pub fn initialize_token_account(ctx: Context<InitializeTokenAccount>) -> Result<()> {
+        // No additional checks needed, token account checks are done by token program
+        msg!("Initialize associated token account if needed");
+        msg!("Associated token account: {}", ctx.accounts.associated_token.key());
         Ok(())
     }
 }
@@ -26,18 +38,29 @@ pub struct Initialize<'info> {
         payer = signer, 
         space = 8 + 1 + 8
     )]
-    pub new_account: Account<'info, NewAccount>,
+    pub new_account: Account<'info, DataAccount>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct InitializeTokenAccount<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(
+        init_if_needed,
+        payer = signer,
+        associated_token::mint = mint,
+        associated_token::authority = signer,
+    )]
+    pub associated_token: Account<'info, TokenAccount>,
+    pub mint: Account<'info, Mint>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
     pub system_program: Program<'info, System>,
 }
 
 #[account]
-pub struct NewAccount {
+pub struct DataAccount {
     is_initialized: bool, // 1 byte
     data: u64, // 8 bytes
-}
-
-#[error_code]
-pub enum MyError {
-    #[msg("Account already Initialized")]
-    AlreadyInitialized,
 }
--- account-constraints/init_if_needed/tests/example.ts
+++ account-constraints/init_if_needed/tests/example.ts
@@ -3,11 +3,17 @@ import { Program } from "@coral-xyz/anchor";
 import { Example } from "../target/types/example";
 import { Keypair, SystemProgram } from "@solana/web3.js";
 import assert from "assert";
+import {
+  createMint,
+  getAccount,
+  getAssociatedTokenAddressSync,
+} from "@solana/spl-token";
 
 describe("example", () => {
   const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
   const wallet = provider.wallet as anchor.Wallet;
+  const connection = provider.connection;
 
   const program = anchor.workspace.Example as Program<Example>;
 
@@ -15,11 +21,12 @@ describe("example", () => {
   const newAccountKp = new Keypair();
 
   // Instruction data
-  const input = new anchor.BN(42);
+  const initialInput = new anchor.BN(42);
+  const updatedInput = new anchor.BN(24);
 
-  it("Is initialized!", async () => {
+  it("Create and initialize account", async () => {
     const transactionSignature = await program.methods
-      .initialize(input)
+      .initialize(initialInput)
       .accounts({
         signer: wallet.publicKey,
         newAccount: newAccountKp.publicKey,
@@ -31,30 +38,77 @@ describe("example", () => {
     console.log("Your transaction signature", transactionSignature);
 
     // Fetch the newly created account
-    const accountData = await program.account.newAccount.fetch(
+    const accountData = await program.account.dataAccount.fetch(
       newAccountKp.publicKey
     );
 
-    assert(accountData.data.eq(input));
+    assert(accountData.data.eq(initialInput));
   });
 
-  it("Already initialized, expect fail", async () => {
-    try {
-      await program.methods
-        .initialize(input)
-        .accounts({
-          signer: wallet.publicKey,
-          newAccount: newAccountKp.publicKey,
-          systemProgram: SystemProgram.programId,
-        })
-        .signers([newAccountKp])
-        .rpc();
-    } catch (error) {
-      // Expect transaction to fail, return custom error message
-      assert.strictEqual(
-        error.error.errorMessage,
-        "Account already Initialized"
-      );
-    }
+  it("Account already initialized, data remains unchanged", async () => {
+    const transactionSignature = await program.methods
+      .initialize(updatedInput)
+      .accounts({
+        signer: wallet.publicKey,
+        newAccount: newAccountKp.publicKey,
+      })
+      .signers([newAccountKp])
+      .rpc();
+
+    console.log("Your transaction signature", transactionSignature);
+
+    // Fetch the newly created account
+    const accountData = await program.account.dataAccount.fetch(
+      newAccountKp.publicKey
+    );
+
+    assert(accountData.data.eq(initialInput));
+    assert.notEqual(accountData.data, updatedInput);
+  });
+
+  it("Initialize Associated Token Account if needed", async () => {
+    // Create new mint account
+    const mint = await createMint(
+      connection,
+      wallet.payer, // payer
+      wallet.publicKey, // mint authority
+      null, // freeze authority
+      2 // decimals
+    );
+
+    // Derive associated token account address
+    const associatedTokenAddress = getAssociatedTokenAddressSync(
+      mint,
+      wallet.publicKey
+    );
+
+    // Invoke initializeTokenAccount to create associated token account
+    const transactionSignature = await program.methods
+      .initializeTokenAccount()
+      .accounts({
+        signer: wallet.publicKey,
+        associatedToken: associatedTokenAddress,
+        mint: mint,
+      })
+      .rpc();
+
+    console.log("Your transaction signature", transactionSignature);
+
+    // Invoke initializeTokenAccount again
+    // Instruction proceeds without error, even though the associated token account already exists
+    const transactionSignature2 = await program.methods
+      .initializeTokenAccount()
+      .accounts({
+        signer: wallet.publicKey,
+        associatedToken: associatedTokenAddress,
+        mint: mint,
+      })
+      .rpc();
+
+    console.log("Your transaction signature", transactionSignature2);
+
+    const tokenAccount = await getAccount(connection, associatedTokenAddress);
+    assert(tokenAccount.mint.toBase58() == mint.toBase58());
+    assert(tokenAccount.owner.toBase58() == wallet.publicKey.toBase58());
   });
 });
