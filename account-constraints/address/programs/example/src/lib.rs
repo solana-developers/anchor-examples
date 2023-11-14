cce228025889a3d5c2a04fbd68013e89303d1af0
John <johnzhengyuliu@gmail.com>
2023-11-13 18:34:10 -0600

    update address example

--- account-constraints/address/programs/example/src/lib.rs
+++ account-constraints/address/programs/example/src/lib.rs
@@ -10,15 +10,57 @@ const HARDCODED_PUBKEY: Pubkey = pubkey!("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZw
 pub mod example {
     use super::*;
 
-    pub fn address_constraint(ctx: Context<AddressConstraint>) -> Result<()> {
-        msg!("Address: {}", ctx.accounts.address.key());
+    pub fn initialize(ctx: Context<Initialize>, input: u64) -> Result<()> {
+        msg!("Initialize data to: {}!", input);
+        ctx.accounts.new_account.data = input;
+        ctx.accounts.new_account.pubkey = ctx.accounts.signer.key();
+        Ok(())
+    }
+
+    pub fn update(ctx: Context<Update>, input: u64) -> Result<()> {
+        msg!("Changed data to: {}!", input);
+        ctx.accounts.existing_account.data = input;
+        Ok(())
+    }
+
+    pub fn hardcoded_address(ctx: Context<HardcodedAddress>) -> Result<()> {
+        msg!("Address: {}", ctx.accounts.account.key());
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct AddressConstraint<'info> {
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(
+        init, 
+        payer = signer, 
+        space = 8 + 8 + 32
+    )]
+    pub new_account: Account<'info, DataAccount>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Update<'info> {
+    #[account(address = existing_account.pubkey)]
+    pub signer: Signer<'info>,
+    #[account(mut)]
+    pub existing_account: Account<'info, DataAccount>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct HardcodedAddress<'info> {
     /// CHECK: address constraint example
     #[account(address = HARDCODED_PUBKEY)]
-    pub address: UncheckedAccount<'info>,
+    pub account: UncheckedAccount<'info>,
+}
+
+
+#[account]
+pub struct DataAccount {
+    data: u64, // 8 bytes
+    pubkey: Pubkey, // 32 bytes
 }
--- account-constraints/address/tests/example.ts
+++ account-constraints/address/tests/example.ts
@@ -5,36 +5,112 @@ import { PublicKey, Keypair } from "@solana/web3.js";
 import assert from "assert";
 
 describe("example", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const wallet = provider.wallet as anchor.Wallet;
 
   const program = anchor.workspace.Example as Program<Example>;
 
-  it("Test address constraint", async () => {
-    // Address that matches the hardcoded address in the program
-    const hardcodedPubkey = new PublicKey(
-      "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"
-    );
+  // Generate keypair for the new account
+  const newAccountKp = new Keypair();
+
+  it("Is initialized!", async () => {
+    // Instruction data
+    const input = new anchor.BN(42);
 
     const transactionSignature = await program.methods
-      .addressConstraint()
+      .initialize(input)
       .accounts({
-        address: hardcodedPubkey,
+        signer: wallet.publicKey,
+        newAccount: newAccountKp.publicKey,
+      })
+      .signers([newAccountKp])
+      .rpc();
+
+    console.log("Your transaction signature", transactionSignature);
+
+    // Fetch the newly created account
+    const accountData = await program.account.dataAccount.fetch(
+      newAccountKp.publicKey
+    );
+
+    assert(accountData.data.eq(input));
+    assert(accountData.pubkey.equals(wallet.publicKey));
+  });
+
+  it("Update account data", async () => {
+    // Instruction data
+    const input = new anchor.BN(24);
+
+    const transactionSignature = await program.methods
+      .update(input)
+      .accounts({
+        signer: wallet.publicKey,
+        existingAccount: newAccountKp.publicKey,
       })
       .rpc();
 
     console.log("Your transaction signature", transactionSignature);
+
+    // Fetch the newly created account
+    const accountData = await program.account.dataAccount.fetch(
+      newAccountKp.publicKey
+    );
+
+    assert(accountData.data.eq(input));
+    assert(accountData.pubkey.equals(wallet.publicKey));
   });
 
   it("Violate address constraint, expect fail", async () => {
     // Random keypair to use as "address" account in the instruction
     const randomKeypair = new Keypair();
 
+    // Instruction data
+    const input = new anchor.BN(24);
+
     try {
       await program.methods
-        .addressConstraint()
+        .update(input)
         .accounts({
-          address: randomKeypair.publicKey,
+          signer: randomKeypair.publicKey,
+          existingAccount: newAccountKp.publicKey,
+        })
+        .signers([randomKeypair])
+        .rpc();
+    } catch (error) {
+      // Expect transaction to fail, since address does not match constraint
+      assert.strictEqual(
+        error.error.errorMessage,
+        "An address constraint was violated"
+      );
+    }
+  });
+
+  it("Test hardcoded address constraint", async () => {
+    // Address that matches the hardcoded address in the program
+    const hardcodedPubkey = new PublicKey(
+      "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"
+    );
+
+    const transactionSignature = await program.methods
+      .hardcodedAddress()
+      .accounts({
+        account: hardcodedPubkey,
+      })
+      .rpc();
+
+    console.log("Your transaction signature", transactionSignature);
+  });
+
+  it("Violate hardcoded address constraint, expect fail", async () => {
+    // Random keypair to use as "address" account in the instruction
+    const randomKeypair = new Keypair();
+
+    try {
+      await program.methods
+        .hardcodedAddress()
+        .accounts({
+          account: randomKeypair.publicKey,
         })
         .rpc();
     } catch (error) {
